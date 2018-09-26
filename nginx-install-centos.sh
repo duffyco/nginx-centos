4b2a3b0900f49ab29ba5dd6f6108f0c5747de2ef
John Duffy <jduffy@cbnco.com>
2018-09-26 15:49:20 -0400

    update

--- nginx-install-centos.sh
+++ nginx-install-centos.sh
@@ -5,5 +5,5 @@ yum -y update
 # Install SELinux management tools and add rule for default port for mongod service
 #semanage port -a -t mongod_port_t -p tcp 27017
 
-yum install epel-release
-yum install nginx
+yum -y install epel-release
+yum -y install nginx
